c1b37e6fb8c71004024ae9c4a089d5902aa91a9b
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-12-18 11:13:54 +0100

    chore: Update to Svelte 5. Update to eslint 9.

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+import * as svelteParser from 'svelte-eslint-parser';
+import * as typescriptParser from '@typescript-eslint/parser';
+import svelteConfig from './svelte.config.js';
+
+export default [
+	js.configs.recommended,
+	...eslintPluginSvelte.configs['flat/recommended'],
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parser: svelteParser,
+			parserOptions: {
+				parser: typescriptParser,
+				project: './tsconfig.json',
+				extraFileExtensions: ['.svelte'],
+				svelteConfig
+			}
+		}
+	},
+	{
+		ignores: [
+			'.DS_Store',
+			'node_modules',
+			'.svelte-kit',
+			'build',
+			'package',
+			'.env',
+			'.env.*',
+			'!.env.example',
+			'pnpm-lock.yaml',
+			'package-lock.json',
+			'yarn.lock'
+		]
+	}
+];
